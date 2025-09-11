10ca2ed890e2cb587234d5e1786530e2fd6e608d
ZenPatel <zenpatel903@gmail.com>
2025-09-11 16:25:28 -0400

    Create SuperMarioBros.tsx

---
+++ src/components/SuperMarioBros.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SuperMarioBros: React.FC = () => {
+  return (
+    <iframe
+      src="/supermario.html"
+      style={{ width: "800px", height: "600px", border: "none" }}
+      title="Super Mario Bros"
+    />
+  );
+};
+
+export default SuperMarioBros;
