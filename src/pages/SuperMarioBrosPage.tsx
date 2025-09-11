8d97c08cd817bbee72151d2201945886a652010b
ZenPatel <zenpatel903@gmail.com>
2025-09-11 16:26:01 -0400

    Create SuperMarioBrosPage.tsx

---
+++ src/pages/SuperMarioBrosPage.tsx
@@ -0,0 +1,9 @@
+import SuperMarioBros from "../components/SuperMarioBros";
+
+export const SuperMarioBrosPage = () => {
+  return (
+    <div className="pt-1">
+      <SuperMarioBros />
+    </div>
+  );
+};
