40703e51eaa5d9f9812b0b9e8e04cdb0f527e7f8
ZenPatel <zenpatel903@gmail.com>
2025-09-11 17:13:29 -0400

    Update SuperMarioBrosPage.tsx

--- src/pages/SuperMarioBrosPage.tsx
+++ src/pages/SuperMarioBrosPage.tsx
@@ -3,6 +3,9 @@ import SuperMarioBros from "../components/SuperMarioBros";
 export const SuperMarioBrosPage = () => {
   return (
     <div className="pt-1">
+      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-red-500 to-blue-300 bg-clip-text text-transparent leading-tight pb-1"> 
+        Super Mario Bros
+      </h2>
       <SuperMarioBros />
     </div>
   );
