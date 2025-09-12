89ef89b1202e84043f4cf0d8ad6badb909ae027a
ZenPatel <zenpatel903@gmail.com>
2025-09-11 22:01:57 -0400

    Update PunchOutPage.tsx

--- src/pages/PunchOutPage.tsx
+++ src/pages/PunchOutPage.tsx
@@ -3,7 +3,7 @@ import PunchOut from "../components/PunchOut";
 export const PunchOutPage = () => {
   return (
     <div className="pt-1">
-      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-green-300 to-yellow-500 bg-clip-text text-transparent leading-tight pb-1"> 
+      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent leading-tight pb-1"> 
         🥊 Punch Out
       </h2>
       <PunchOut />
