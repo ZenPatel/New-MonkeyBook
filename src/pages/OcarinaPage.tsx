bd114d112a49ba0f419d21bc40a9b24f85d19cc4
ZenPatel <zenpatel903@gmail.com>
2025-09-11 22:00:29 -0400

    Update OcarinaPage.tsx

--- src/pages/OcarinaPage.tsx
+++ src/pages/OcarinaPage.tsx
@@ -4,7 +4,7 @@ export const OcarinaPage = () => {
   return (
     <div className="pt-1">
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-green-300 to-yellow-500 bg-clip-text text-transparent leading-tight pb-1"> 
-        LoZ Ocarina of Time
+        🪈 LoZ Ocarina of Time
       </h2>
       <Ocarina />
     </div>
