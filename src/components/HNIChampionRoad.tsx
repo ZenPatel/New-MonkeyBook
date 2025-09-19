9de8ecc93dbe6d63961e913fbeb87ac20e3298a7
ZenPatel <zenpatel903@gmail.com>
2025-09-19 01:38:06 +0000

    Testing S3 Bucket functionality

--- src/components/HNIChampionRoad.tsx
+++ src/components/HNIChampionRoad.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ShowPlayer from "./ShowPlayer";
-import Video from "../assets/bandicam 2025-04-04 01-40-08-386.mp4"
 
 export const HNIChampionRoad: React.FC = () => {
   return (
-    <ShowPlayer src={Video} title="HNIChampionRoad"/>
+    <ShowPlayer src="https://monkeybookshows.s3.us-east-2.amazonaws.com/bandicam+2024-12-08+14-20-51-972.mp4" title="HNIChampionRoad"/>
   );
 };
