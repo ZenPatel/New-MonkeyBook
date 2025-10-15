abd89113a048cc0b640c6ac2b4014f2895c54562
ZenPatel <zenpatel903@gmail.com>
2025-10-15 22:10:12 +0000

    Debug

--- src/pages/FlappyBirdPage.tsx
+++ src/pages/FlappyBirdPage.tsx
@@ -3,7 +3,7 @@ import { FlappyBird } from "../components/FlappyBird";
 export const FlappyBirdPage = () => {
   return (
     <div className="pt-1">
-      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-red-500 to-blue-300 bg-clip-text text-transparent leading-tight pb-1"> 
+      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-yellow-300 to-red-500 bg-clip-text text-transparent leading-tight pb-1"> 
         🐦 Flappy Bird
       </h2>
       <FlappyBird />
--- src/pages/SuperhotPage.tsx
+++ src/pages/SuperhotPage.tsx
@@ -3,7 +3,7 @@ import { Superhot } from "../components/Superhot";
 export const SuperhotPage = () => {
   return (
     <div className="pt-1">
-      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent leading-tight pb-1"> 
+      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-red-500 to-red-300 bg-clip-text text-transparent leading-tight pb-1"> 
         🔫 Superhot
       </h2>
       <p className="text-sm text-gray-500 italic text-center">
