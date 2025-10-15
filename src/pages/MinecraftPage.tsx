dcc7276a555c3dc313aff6908e82711faa2abef5
ZenPatel <zenpatel903@gmail.com>
2025-10-15 21:15:36 +0000

    Debug on Minecraft

--- src/pages/MinecraftPage.tsx
+++ src/pages/MinecraftPage.tsx
@@ -6,9 +6,6 @@ export const MinecraftPage = () => {
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-orange-650 bg-clip-text text-transparent leading-tight pb-1"> 
         ⛏️ Minecraft
       </h2>
-      <p className="text-sm text-gray-500 italic text-center">
-        Press -= for fullscreen
-      </p>
       <Minecraft />
     </div>
   );
