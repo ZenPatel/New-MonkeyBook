c6f08e76d29e6c3de49ec8b62b4c8a6c14f6a504
ZenPatel <zenpatel903@gmail.com>
2025-10-15 21:28:21 +0000

    Another MC debug

--- src/components/Minecraft.tsx
+++ src/components/Minecraft.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Minecraft: React.FC = () => {
   return (
     <div className="grid place-items-center h-screen">
-      <iframe src="https://tylerpalko.github.io/gamehub/minecraft1.5.2/" title="Minecraft" height="600" width="100%" className="border rounded-lg" allowFullScreen allow="fullscreen"/>
+      <iframe src="https://eaglercraftminecraft.org/" title="Minecraft" height="600" width="100%" className="border rounded-lg" allowFullScreen allow="fullscreen"/>
     </div>
   );
 };
