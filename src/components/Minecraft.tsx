189c04a967b77b0ec6035e3a42a8f2f243361114
ZenPatel <zenpatel903@gmail.com>
2025-10-29 04:00:08 +0000

    Improved Minecraft

--- src/components/Minecraft.tsx
+++ src/components/Minecraft.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Minecraft: React.FC = () => {
   return (
     <div className="grid place-items-center h-screen">
-      <iframe src="https://eaglercraftminecraft.org/" title="Minecraft" height="600" width="100%" className="border rounded-lg" allowFullScreen allow="fullscreen"/>
+      <iframe src="https://eaglercraftminecraft.org/game/eaglercraft/" title="Minecraft" height="600" width="100%" className="border rounded-lg" allowFullScreen allow="fullscreen"/>
     </div>
   );
 };
--- src/pages/MinecraftPage.tsx
+++ src/pages/MinecraftPage.tsx
@@ -6,9 +6,6 @@ export const MinecraftPage = () => {
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-orange-650 bg-clip-text text-transparent leading-tight pb-1"> 
         ⛏️ Minecraft
       </h2>
-      <h4 className="text-1xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-orange-650 bg-clip-text text-transparent leading-tight pb-1"> 
-        Powered by: Eaglercraft X
-      </h4>
       <Minecraft />
     </div>
   );
